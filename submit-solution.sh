7026d08d5adf349ad9a8abbdff867bb183c2af5f
AlazzR <rasheedalazzoni1@gmail.com>
2024-06-04 20:11:33 -0400

    ./submit-solution.sh

--- submit-solution.sh
+++ submit-solution.sh
@@ -3,4 +3,4 @@ npm run predeploy
 # Pass the commit message as the first argument
 git add . && git commit -m "$0"
 git subtree push --prefix dist origin gh-pages
-git push origin masterecho "First arg: $1"
+git push origin master
